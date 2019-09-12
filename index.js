482dcec270707af45d7680bd3eb4743aad4da1f0
instafluff <instaflufftv@gmail.com>
2019-09-12 09:30:20 -0700

    Use ComfyDB and Update!

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
 
+var ComfyDB = require( "comfydb" );
+
 var PORT = process.env.PORT || 9001;
 var express = require( 'express' );
+var cors = require('cors');
 var app = express();
+app.use(cors());
 
 // API Key Creation via Twitch Login!
 // - CreateAPIKey
@@ -32,105 +36,116 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.get("/games/:gameId", (req, res) => {
-  db.get( `SELECT * FROM games WHERE gameId="${req.params.gameId}"`, ( err, row ) => {
-    if( err ) { return res.status( 403 ).json( { error: err } ); }
-    if( row ) { return res.json( row ); }
-    return res.status( 403 ).json( { error: "Not Found" } );
-  } );
+app.get("/games/:gameId", async (req, res) => {
+  var game = await ComfyDB.Data.FindByKey( "games", req.params.gameId );
+  return res.json( game );
 });
 
-app.get("/games/:gameId/scores/:leaderboardId", (req, res) => {
-  db.get( `SELECT * FROM leaderboards WHERE gameId="${req.params.gameId}" AND leaderboardId="${req.params.leaderboardId}"`, ( err, row ) => {
-    if( err ) { return res.status( 403 ).json( { error: err } ); }
-    if( row ) { return res.json( row ); }
-    return res.status( 403 ).json( { error: "Not Found" } );
+app.get("/games/:gameId/scores/:leaderboardId", async (req, res) => {
+  // TODO: Find and get leaderboard asc/desc setting
+  var scores = await ComfyDB.Data.Find( `scores/${req.params.gameId}/${req.params.leaderboardId}`, {
+    sortBy: "score",
+    isOrderDescending: true
   } );
+  return res.json( scores );
 });
 
-app.get("/games/:gameId/scores/:leaderboardId/top", (req, res) => {
-  db.get( `SELECT * FROM leaderboards WHERE gameId="${req.params.gameId}" AND leaderboardId="${req.params.leaderboardId}"`, ( err, row ) => {
-    if( err ) { return res.status( 403 ).json( { error: err } ); }
-    if( row ) {
-      var isAsc = row.isAsc;
-      db.all( `SELECT id,player,score,created FROM scores WHERE gameId="${req.params.gameId}" AND leaderboardId="${req.params.leaderboardId}" ORDER BY score ${isAsc > 0 ? "ASC" : "DESC"} LIMIT 100`, ( err, row ) => {
-        if( err ) { return res.status( 403 ).json( { error: err } ); }
-        return res.json( row );
-      } );
-      return;
-    }
-    return res.status( 403 ).json( { error: "Not Found" } );
+app.get("/games/:gameId/scores/:leaderboardId/top", async (req, res) => {
+  // TODO: Find and get leaderboard asc/desc setting
+  var scores = await ComfyDB.Data.Find( `scores/${req.params.gameId}/${req.params.leaderboardId}`, {
+    sortBy: "score",
+    count: 3,
+    isOrderDescending: true
   } );
+  return res.json( scores );
 });
 
-app.listen( PORT );
+app.get("/games/:gameId/scores/:leaderboardId/:player/:score", async (req, res) => {
+  postScore( req.params.gameId, req.params.leaderboardId, req.params.player, parseFloat( req.params.score ) );
+  return res.json( {} );
+});
 
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+app.get("/games/:gameId/scores/:leaderboardId/delete", async (req, res) => {
+  clearLeaderboard( req.params.gameId, req.params.leaderboardId );
+  return res.json( {} );
+});
 
-db.serialize(function() {
+Initialize();
+
+async function Initialize() {
   try {
-    db.run( "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, gameId CHAR(32) UNIQUE NOT NULL, title TEXT NOT NULL, desc TEXT, created DATETIME, updated DATETIME)" );
-    db.run( "CREATE TABLE IF NOT EXISTS leaderboards (id INTEGER PRIMARY KEY, gameId CHAR(32) NOT NULL, leaderboardId CHAR(32) NOT NULL, isAsc BOOLEAN NOT NULL, title TEXT NOT NULL, desc TEXT, created DATETIME, updated DATETIME, UNIQUE(gameId, leaderboardId))" );
-    db.run( "CREATE TABLE IF NOT EXISTS scores (id INTEGER PRIMARY KEY, gameId CHAR(32) NOT NULL, leaderboardId CHAR(32) NOT NULL, player CHAR(100) NOT NULL, score NUMERIC NOT NULL, created DATETIME)" );
-    //
-    // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    // for (var i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    // }
-    // stmt.finalize();
-    //
-    // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    //     console.log(row.id + ": " + row.info);
-    // });
+    await ComfyDB.Init();
+    await ComfyDB.Collections.Create( "games" );
+    // await ComfyDB.Collections.Create( "achievements" );
+    await ComfyDB.Collections.Create( "leaderboards" );
+    createGame( "penguindrop", "Penguin Drop", "!drop to try and land on the target for a high score!" );
     createGame( "testgame", "Test Game!", "This is a test game" );
     createGame( "testgame2", "Test Game 2!", "This is the #2 test game" );
+    createLeaderboard( "penguindrop", "leaderboard", "Penguin Drop High Scores!", "Best Target Landing Evarrrrr" );
     createLeaderboard( "testgame", "leaderboard1", "Test Leaderboard", "I wanna be the very best like no one ever was." );
     createLeaderboard( "testgame", "leaderboard2", "Test Leaderboard 2", "To catch them is my real test. To train them is my cause." );
     postScore( "testgame", "leaderboard1", "instafluff", 5.276 );
     postScore( "testgame", "leaderboard1", "instafluff1", 2.276 );
     postScore( "testgame", "leaderboard1", "instafluff2", 5.35 );
     postScore( "testgame", "leaderboard1", "instafluff3", -5.276 );
-    // db.each( "SELECT * FROM games", ( err, row ) => {
-    //   console.log( row );
-    // } );
-    // db.each( "SELECT * FROM leaderboards", ( err, row ) => {
-    //   console.log( row );
-    // } );
-    // db.each( "SELECT * FROM scores ORDER BY score DESC", ( err, row ) => {
-    //   console.log( row );
-    // } );
+    // clearLeaderboard( "testgame", "leaderboard1" );
+    // postScore( "testgame", "leaderboard1", "instafluff3", -5.276 );
+    app.listen( PORT );
   }
-  catch( err ) {
-    console.log( "ERROR:", err );
+  catch( ex ) {
+    console.error( ex );
   }
-});
+}
 
-function createGame( gameId, title, description = "" ) {
+async function createGame( gameId, title, description = "" ) {
   try {
-    db.run( `INSERT INTO games (gameId, title, desc, created, updated) VALUES ("${gameId.toLowerCase()}", "${title}", "${description}", DATETIME(), DATETIME())` );
+    await ComfyDB.Data.SetByKey( "games", gameId, { gameId, title, description } );
   }
   catch( err ) {
     console.log( "Create Game Failed:", err );
   }
 }
 
-function createLeaderboard( gameId, leaderboardId, title, description, isAscending = false ) {
+async function createLeaderboard( gameId, leaderboardId, title, description, isAscending = false ) {
   try {
-    db.run( `INSERT INTO leaderboards (gameId, leaderboardId, isAsc, title, desc, created, updated) VALUES ("${gameId.toLowerCase()}", "${leaderboardId.toLowerCase()}", ${isAscending}, "${title}", "${description}", DATETIME(), DATETIME())` );
+    // TODO: Check if game exists
+    await ComfyDB.Data.SetByKey( "leaderboards", `${gameId}/${leaderboardId}`, { gameId, leaderboardId, title, description, isAscending } );
+    await ComfyDB.Collections.Create( `scores/${gameId}/${leaderboardId}` );
   }
   catch( err ) {
     console.log( "Create Game Failed:", err );
   }
 }
 
-function postScore( gameId, leaderboardId, player, score ) {
+async function clearLeaderboard( gameId, leaderboardId ) {
   try {
-    db.run( `INSERT INTO scores (gameId, leaderboardId, player, score, created) VALUES ("${gameId.toLowerCase()}", "${leaderboardId.toLowerCase()}", "${player}", ${score}, DATETIME())` );
+    // TODO: Check if game exists
+    // TODO: Check if leaderboard exists
+    await ComfyDB.Collections.Delete( `scores/${gameId}/${leaderboardId}` );
+    await ComfyDB.Collections.Create( `scores/${gameId}/${leaderboardId}` );
   }
   catch( err ) {
     console.log( "Create Game Failed:", err );
   }
 }
 
-// db.close();
+async function postScore( gameId, leaderboardId, player, score ) {
+  try {
+    // TODO: Check if the game exists
+    // TODO: Check if the leaderboard exists
+    // TODO: Check if score is lower than before on ascending leaderboards
+    // Check if score is higher than before
+    var prevScore = await ComfyDB.Data.FindByKey( `scores/${gameId}/${leaderboardId}`, player );
+    if( prevScore.length > 0 ) {
+      if( prevScore[ 0 ].score < score ) {
+        await ComfyDB.Data.SetByKey( `scores/${gameId}/${leaderboardId}`, player, { gameId, leaderboardId, player, score } );
+      }
+    }
+    else {
+      await ComfyDB.Data.SetByKey( `scores/${gameId}/${leaderboardId}`, player, { gameId, leaderboardId, player, score } );
+    }
+  }
+  catch( err ) {
+    console.log( "Create Game Failed:", err );
+  }
+}
